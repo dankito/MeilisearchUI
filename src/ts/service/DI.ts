c338ce1722a0515d4b76eae3e8826ef43fae3433
dankito <git@dankito.net>
2026-06-03 00:41:29 +0200

    Added fetch based WebClient implementation

--- src/ts/service/DI.ts
+++ src/ts/service/DI.ts
@@ -1,7 +1,11 @@
+import type {WebClient} from "../clients/web/WebClient"
+import {FetchWebClient} from "../clients/web/FetchWebClient"
 import {LogService} from "./LogService"
 
 export class DI {
 
   static readonly log: LogService = new LogService()
+
+  static readonly webClient: WebClient = new FetchWebClient(null, DI.log)
 
 }
