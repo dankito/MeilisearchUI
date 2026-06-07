ec58e8c376afe607cdc9a7c52297876fd9e72769
dankito <git@dankito.net>
2026-06-07 22:16:52 +0200

    Fixed setting offset

--- src/ts/service/MeiliService.ts
+++ src/ts/service/MeiliService.ts
@@ -11,7 +11,8 @@ export class MeiliService {
 
 
   async search(indexName: string, search: SearchState): Promise<SearchResponse> {
-    const searchQuery: MultiSearchQuery = { indexUid: indexName, q: search.query, limit: search.pageSize, offset: search.page, matchingStrategy: search.matchingStrategy }
+    const offset = search.page * search.pageSize
+    const searchQuery: MultiSearchQuery = { indexUid: indexName, q: search.query, limit: search.pageSize, offset: offset, matchingStrategy: search.matchingStrategy }
     const params: MultiSearchParams = { queries: [ searchQuery ] }
     const response: MultiSearchResponse = await this.client.multiSearch(params)
 
